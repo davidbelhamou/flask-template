43919b515d161fdfad69fa815a6771d3016a4c47
davidbelhamou <53448151+davidbelhamou@users.noreply.github.com>
2024-07-24 09:43:24 +0300

    Update myApp.py

--- flaskWrapper/myApp.py
+++ flaskWrapper/myApp.py
@@ -1,7 +1,11 @@
 from wrapper import MyWebApp
+webapp = MyWebApp('myapp')
+app = webapp.app
 
+def main():
+    webapp.run()
+    print("app is running)
 
 if __name__ == '__main__':
-    webApp = MyWebApp('MyApp')
-    webApp.run()
-    print('webApp is running')
+    main()
+    
